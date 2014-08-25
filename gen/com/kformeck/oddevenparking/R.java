e60971bb689079384878bb5500fe1f307a0032ab
kformeck89 <kformeck@gmail.com>
2014-08-25 18:08:32 -0400

    Re-work the MainActivity

--- gen/com/kformeck/oddevenparking/R.java
+++ gen/com/kformeck/oddevenparking/R.java
@@ -113,17 +113,19 @@ containing a value of this type.
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f06000a;
-        public static final int fab_margin_bottom=0x7f060001;
-        public static final int fab_margin_right=0x7f060002;
-        public static final int font_size_body=0x7f060006;
-        public static final int font_size_subheader=0x7f060007;
-        public static final int font_size_title=0x7f060008;
-        public static final int horizontal_content_margin=0x7f060004;
-        public static final int horizontal_content_margin_edittext=0x7f060005;
-        public static final int item_text_left_margin=0x7f060000;
-        public static final int standard_content_margin=0x7f060003;
-        public static final int trigger_type_button_size=0x7f060009;
+        public static final int activity_horizontal_margin=0x7f06000c;
+        public static final int fab_margin_side=0x7f060002;
+        public static final int fab_margin_top=0x7f060003;
+        public static final int fab_size=0x7f060004;
+        public static final int font_size_body=0x7f060008;
+        public static final int font_size_subheader=0x7f060009;
+        public static final int font_size_title=0x7f06000a;
+        public static final int horizontal_content_margin=0x7f060006;
+        public static final int horizontal_content_margin_edittext=0x7f060007;
+        public static final int item_text_left_margin=0x7f060001;
+        public static final int standard_content_margin=0x7f060005;
+        public static final int top_sheet_height_main=0x7f060000;
+        public static final int trigger_type_button_size=0x7f06000b;
     }
     public static final class drawable {
         public static final int ic_clock=0x7f020000;
@@ -133,13 +135,15 @@ containing a value of this type.
         public static final int ripple_primary=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0006;
-        public static final int btnAddTrigger=0x7f0a0000;
-        public static final int btnLocation=0x7f0a0002;
-        public static final int btnTime=0x7f0a0003;
-        public static final int content=0x7f0a0005;
-        public static final int thing=0x7f0a0004;
-        public static final int txtTriggerTitle=0x7f0a0001;
+        public static final int action_settings=0x7f0a0008;
+        public static final int btnAddTrigger=0x7f0a0002;
+        public static final int btnLocation=0x7f0a0004;
+        public static final int btnTime=0x7f0a0005;
+        public static final int content=0x7f0a0007;
+        public static final int thing=0x7f0a0006;
+        public static final int topSheet=0x7f0a0000;
+        public static final int txtReminderText=0x7f0a0001;
+        public static final int txtTriggerTitle=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -154,10 +158,13 @@ containing a value of this type.
         public static final int action_settings=0x7f070004;
         public static final int app_name=0x7f070000;
         public static final int hint_new_trigger=0x7f070001;
-        public static final int location_button_desc=0x7f070007;
-        public static final int radiobutton_location=0x7f070005;
-        public static final int radiobutton_time=0x7f070006;
-        public static final int time_button_desc=0x7f070008;
+        public static final int location_button_desc=0x7f07000a;
+        public static final int radiobutton_location=0x7f070008;
+        public static final int radiobutton_time=0x7f070009;
+        public static final int reminder_text_default=0x7f070006;
+        public static final int reminder_text_post=0x7f070007;
+        public static final int reminder_text_pre=0x7f070005;
+        public static final int time_button_desc=0x7f07000b;
         public static final int title=0x7f070003;
         public static final int triggers=0x7f070002;
     }
--- src/com/kformeck/oddevenparking/MainActivity.java
+++ src/com/kformeck/oddevenparking/MainActivity.java
@@ -4,15 +4,24 @@ import com.kformeck.widgets.FloatingActionButton;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Explode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 
 public class MainActivity extends Activity {
 	private Context context;
 	private FloatingActionButton btnAddTrigger;
+	private TranslateAnimation fabTranlateAnimation;
+	private ScaleAnimation fabShrinkAnimation;
+	private int windowHeight;
+	private int windowWidth;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +29,38 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		context = this;
 		
+		windowHeight = getBaseContext().getResources().getDisplayMetrics().heightPixels;
+		windowWidth = getBaseContext().getResources().getDisplayMetrics().widthPixels;
+		
 		btnAddTrigger = (FloatingActionButton) findViewById(R.id.btnAddTrigger);
 		btnAddTrigger.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Intent triggerIntent = new Intent(context, NewTriggerActivity.class);
-				startActivity(triggerIntent);
+				view.startAnimation(fabTranlateAnimation);
 			}
 		});
+		
+		int resourceId = context.getResources()
+								.getIdentifier("navigation_bar_height", "dimen", "android");
+		float fabSize = context.getResources().getDimension(R.dimen.fab_size);
+		float navbarHeight = resourceId > 0 ?
+				context.getResources().getDimensionPixelSize(resourceId) : 0;
+		final float movementInX = (windowWidth / 2) - (fabSize / 2) -
+				context.getResources().getDimension(R.dimen.fab_margin_side);
+		final float movementInY = windowHeight - 
+				context.getResources().getDimension(R.dimen.top_sheet_height_main) -
+				fabSize - navbarHeight;
+		
+		
+		fabTranlateAnimation = new TranslateAnimation(
+				0, movementInX, 0, movementInY);
+		fabTranlateAnimation.setDuration(1000);
+		fabTranlateAnimation.setFillAfter(true);
+		
+		fabShrinkAnimation = new ScaleAnimation(
+				1, 0, 1, 0);
+		fabShrinkAnimation.setDuration(1000);
+		fabShrinkAnimation.setFillAfter(true);
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
